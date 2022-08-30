e0cfb8587a4c77ef1da8d3f207e40153766dab34
337yj <110447844+337yj@users.noreply.github.com>
2022-08-31 02:19:43 +0900

    Delete JavaScript-practice directory

--- JavaScript-practice/나이계산/나이계산/main.js
+++
@@ -1,9 +0,0 @@
-// const calc = document.querySelector('.btn');
-
-document.querySelector('.btn').addEventListener('click', function(){
-    let currentYear = 2022;
-    let birthYear = prompt("태어난 년도를 입력하세요.", "YYYY");
-    let age; 
-    age = currentYear - birthYear + 1;
-    document.querySelector("#result").innerHTML = (`당신의 나이는 ${age}세 입니다.`);
-});
